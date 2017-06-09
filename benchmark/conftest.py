889f53ede2b5482cd5dd1e5d71f11e50abf1b329
Alexey Popravka <alexey.popravka@horsedevel.com>
2017-06-09 18:03:41 +0300

    few updates

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 import redis
+import io
+
+from unittest import mock
+from hiredis import Reader as HiReader
+from aioredis.parser import PyReader
 from redis.connection import HiredisParser, PythonParser
 
 
@@ -9,3 +14,50 @@ def redispy(request):
     r = redis.StrictRedis(connection_pool=pool)
     assert r.flushdb() is True
     return r
+
+
+class FakeSocket(io.BytesIO):
+
+    def recv(self, size):
+        return self.read(size)
+
+    def recv_into(self, buf):
+        return self.readinto(buf)
+
+
+class PythonParserReader:
+    Parser = PythonParser
+
+    def __init__(self, encoding=None):
+        self._sock = FakeSocket()
+        self._parser = self.Parser(2**17)
+        self._parser.on_connect(
+            mock.Mock(_sock=self._sock,
+                      decode_responses=encoding is not None,
+                      encoding=encoding))
+
+    def feed(self, data):
+        if not self._sock.tell():
+            self._sock.write(data)
+        self._sock.seek(0)
+
+    def gets(self):
+        return self._parser.read_response()
+
+
+class HiredisParserReader(PythonParserReader):
+    Parser = HiredisParser
+
+
+@pytest.fixture(params=[
+    HiReader(), HiReader(encoding='utf-8'),
+    PyReader(), PyReader(encoding='utf-8'),
+    PythonParserReader(), PythonParserReader(encoding='utf-8'),
+    HiredisParserReader(), HiredisParserReader(encoding='utf-8'),
+], ids=[
+    'hiredis', 'hiredis(utf-8)',
+    'aioredis-python', 'aioredis-python(utf-8)',
+    'redispy-python', 'redispy-python(utf-8)',
+    'redispy-hiredis', 'redispy-hiredis(utf-8)'])
+def reader(request):
+    return request.param
--- benchmark/parsers.py
+++ benchmark/parsers.py
@@ -1,9 +1,5 @@
 import pytest
 
-from hiredis import Reader as HiReader
-from aioredis.parser import PyReader
-# from redis.connection import HiredisParser, PythonParser
-
 
 def feed_and_gets(reader, data):
     reader.feed(data)
@@ -11,23 +7,11 @@ def feed_and_gets(reader, data):
 
 
 @pytest.mark.benchmark(group='simple-string')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_simple_string(benchmark, reader):
     benchmark(feed_and_gets, reader, b'+OK\r\n')
 
 
 @pytest.mark.benchmark(group='simple-error')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_simple_error(benchmark, reader):
     benchmark(feed_and_gets, reader, b'-Error\r\n')
 
@@ -43,44 +27,20 @@ BULK_STR_32K = data(2**15)
 
 
 @pytest.mark.benchmark(group='bulk-string-1K')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_bulk_string_1K(benchmark, reader):
     benchmark(feed_and_gets, reader, BULK_STR_1K)
 
 
 @pytest.mark.benchmark(group='bulk-string-4K')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_bulk_string_4K(benchmark, reader):
     benchmark(feed_and_gets, reader, BULK_STR_4K)
 
 
 @pytest.mark.benchmark(group='bulk-string-16K')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_bulk_string_16K(benchmark, reader):
     benchmark(feed_and_gets, reader, BULK_STR_16K)
 
 
 @pytest.mark.benchmark(group='bulk-string-32K')
-@pytest.mark.parametrize('reader', [
-    HiReader(),
-    HiReader(encoding='utf-8'),
-    PyReader(),
-    PyReader(encoding='utf-8'),
-], ids='hiredis,hiredis(utf-8),pyreader,pyreader(utf-8)'.split(','))
 def benchmark_parser_bulk_string_32K(benchmark, reader):
     benchmark(feed_and_gets, reader, BULK_STR_32K)
